3cc3eff9af4aed47c88a726c4c02d3c5ac858bf2
prajwalraob <33778402+prajwalraob@users.noreply.github.com>
2020-12-09 19:43:18 +0530

    Updated

--- trial_project/newapp/admin.py
+++ trial_project/newapp/admin.py
@@ -2,4 +2,10 @@ from django.contrib import admin
 from .models import Post
 # Register your models here.
 
-admin.site.register(Post)
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'slug', 'author', 'publish', 'status')
+    list_filter = ('status', 'created', 'publish', 'author')
+    search_fields = ('title', 'body')
+    prepopulated_fields = {'slug': ('title',)}
